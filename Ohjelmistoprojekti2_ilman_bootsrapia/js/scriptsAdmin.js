08139a5b8c7ffb3e1b7491880d50b27b32641828
millalaurasvaara <millalaurasvaara@users.noreply.github.com>
2017-04-26 15:26:44 +0300

    Ilman bootstrapia

---
+++ Ohjelmistoprojekti2_ilman_bootsrapia/js/scriptsAdmin.js
@@ -0,0 +1,62 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8B1aUpZFP69o6AB1w5p5JRAo52vnm0VE",
+    authDomain: "all-rounders-newsfeed.firebaseapp.com",
+    databaseURL: "https://all-rounders-newsfeed.firebaseio.com",
+    storageBucket: "all-rounders-newsfeed.appspot.com",
+    messagingSenderId: "151133491596"
+  };
+  firebase.initializeApp(config);
+
+  //oma skripti
+
+  var headText = document.getElementById("otsikko");
+  var mainText = document.getElementById("viesti") ;
+  var submitBtn = document.getElementById("submitBtn");
+  var deleteBtn = document.getElementById("deleteBtn");
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+
+  function submitClick() {
+
+  	// tekstin tallennus
+
+  	var firebaseRef = firebase.database().ref();
+
+  	var teksti = mainText.value;
+    var otsikko = headText.value;
+	var date = new Date();
+
+        //Lisää viestin tieetokantaan
+        firebaseRef.child("viestit").push().set({
+
+        //Viestin otsikko
+        otsikko: date.getDate() +"."+(date.getMonth()+1),
+        //Viestin sisältö
+        viesti: teksti
+        });
+
+  }
+
+  // Viestin poisto
+  function deleteClick(id) {
+	var firebaseRef = firebase.database().ref("viestit/"+id);
+
+	firebaseRef.remove();
+
+    location.reload();
+	}
+
+  var rootRef = firebase.database().ref().child('viestit');
+
+  //Lisää viestin näkymään
+	rootRef.on('child_added', snap =>
+		{ var viesti = snap.child("viesti").val();
+		var uutinen = snap.child("otsikko").val();
+    //Lisää viestin tauluun
+			$("#messages").append("<div class='panel-heading' id='viestiotsikko'>"+uutinen+"</div>" +
+			"<div class='panel-body' id='viestiketju'>" + viesti +"</div>");
+      //Lisää posto nappulan
+      $("#messages").append("<button class='btn btn-danger'  onclick=deleteClick(&quot"+snap.key+"&quot)>Delete</button>");
+		});
---
+++ Ohjelmistoprojekti2_ilman_bootsrapia/js/scriptsUser.js
@@ -0,0 +1,25 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8B1aUpZFP69o6AB1w5p5JRAo52vnm0VE",
+    authDomain: "all-rounders-newsfeed.firebaseapp.com",
+    databaseURL: "https://all-rounders-newsfeed.firebaseio.com",
+    storageBucket: "all-rounders-newsfeed.appspot.com",
+    messagingSenderId: "151133491596"
+  };
+  firebase.initializeApp(config);
+
+  //oma skripti
+
+  var headText = document.getElementById("otsikko");
+  var mainText = document.getElementById("viesti") ;
+
+
+  var rootRef = firebase.database().ref().child('viestit');
+
+	rootRef.on('child_added', snap =>
+		{ var viesti = snap.child("viesti").val();
+		var uutinen = snap.child("otsikko").val();
+			$("#kokonaisuus").append("<p><span><div id='viestiotsikko'>"+uutinen+"</div>" +
+			"<div id='viestiketju'>" + viesti +"</div></span></p>");
+		});
